960fcf44b642919e67adb60ba7c7bb12acc693c3
sarvagya-ui <sarvagya.pandey@outlook.com>
2021-06-03 03:43:15 +0530

    commitTillTodos

--- src/app/MyComponent/todos/todos.component.ts
+++ src/app/MyComponent/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo} from '../../Todo';
+import { Title } from '@angular/platform-browser';
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
@@ -8,8 +9,29 @@ import {Todo} from '../../Todo';
 })
 export class TodosComponent implements OnInit {
 
-  todos:Todo[];
-  constructor() { }
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "This is title2",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 3,
+        title: "This is title3",
+        desc: "Description",
+        active: true
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
